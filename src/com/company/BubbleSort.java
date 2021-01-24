2b6237f2562301ff62d85f1b9c93a77da7920394
Oleksandr Abasov <oleksandra@tradeix.com>
2021-01-24 17:13:57 +0200

    home task

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class BubbleSort {
+
+    /*
+    * Реализуйте алгоритм сортировки пузырьком для сортировки массива
+     */
+    public static int[] sort(int[] array) {
+
+        return null;
+    }
+}
